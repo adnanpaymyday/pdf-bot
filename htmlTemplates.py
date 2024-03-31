a9261ec46dc58297a579492189467a884d838aae
AbhinavS17 <150343481+AbhinavS17@users.noreply.github.com>
2024-03-31 17:44:39 +0000

    htmltemplate updated

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="/sasuke.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="/workspaces/pdf-bot/sasuke.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="/naruto.jpg">
+        <img src="/workspaces/pdf-bot/naruto.jpg">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
